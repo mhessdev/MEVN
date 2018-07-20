5141b29458d21244b38c9f09f003431f60e45b4a
MarcH <mhess@nbaa.org>
2018-07-20 16:20:10 -0400

    CRUD works - useing mongodb atlas

--- server/src/app.js
+++ server/src/app.js
@@ -8,13 +8,87 @@ app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
+// Fetch all posts
 app.get('/posts', (req, res) => {
-  res.send(
-    [{
-      title: "Hello World!",
-      description: "Hi there! How are you? IM Good"
-    }]
-  )
+  Post.find({}, 'title description', function (error, posts) {
+    if (error) { console.error(error); }
+    res.send({
+      posts: posts
+    })
+  }).sort({_id:-1})
 })
 
 app.listen(process.env.PORT || 8083)
+
+// THIS HAS TO DO WITH CONNECTING TO MongoDB -- No fucking clue what it means/does tbh
+var mongoose = require('mongoose');
+mongoose.connect("mongodb+srv://dev:rebel5534@cluster0-46ksk.mongodb.net/posts?retryWrites=true");
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error"));
+db.once("open", function(callback){
+  console.log("Connection Succeeded");
+});
+
+var Post = require("../models/post");
+
+// Add new post
+app.post('/posts', (req, res) => {
+  var db = req.db;
+  var title = req.body.title;
+  var description = req.body.description;
+  var new_post = new Post({
+    title: title,
+    description: description
+  })
+
+  new_post.save(function (error) {
+    if (error) {
+      console.log(error)
+    }
+    res.send({
+      success: true,
+      message: 'Post saved successfully!'
+    })
+  })
+})
+// Fetch single post
+app.get('/post/:id', (req, res) => {
+  var db = req.db;
+  Post.findById(req.params.id, 'title description', function (error, post) {
+    if (error) { console.error(error); }
+    res.send(post)
+  })
+})
+
+// Update a post
+app.put('/posts/:id', (req, res) => {
+  var db = req.db;
+  Post.findById(req.params.id, 'title description', function (error, post) {
+    if (error) { console.error(error); }
+
+    post.title = req.body.title
+    post.description = req.body.description
+    post.save(function (error) {
+      if (error) {
+        console.log(error)
+      }
+      res.send({
+        success: true
+      })
+    })
+  })
+})
+
+// Delete a post
+app.delete('/posts/:id', (req, res) => {
+  var db = req.db;
+  Post.remove({
+    _id: req.params.id
+  }, function(err, post){
+    if (err)
+      res.send(err)
+    res.send({
+      success: true
+    })
+  })
+})
